4306f2e0060424fc411a4c91216be4848210ae37
Ryan Millett <ryan.millett@74bravo.com>
2024-09-21 14:17:11 -0400

    adding auth method and updates to devops-config-schema

--- src/PowerPlatformCLI/PowerPlatformCLI.New/Extension/.openstrata/scripts/pac-shortcuts.ps1
+++ src/PowerPlatformCLI/PowerPlatformCLI.New/Extension/.openstrata/scripts/pac-shortcuts.ps1
@@ -4,6 +4,167 @@ if ([System.IO.File]::Exists("$PSScriptRoot\secrets.ps1"))
     . $PSScriptRoot\secrets.ps1
 }
 
+$Global:OSRootPath = (get-item $PSScriptRoot ).parent.parent.FullName
+$Global:OSEnvJsonPath = "$Global:OSRootPath\environments.json"
+
+function global:get-osenvjson
+{
+    if (Test-Path $Global:OSEnvJsonPath){
+           Get-Content -Path $Global:OSEnvJsonPath -Raw | ConvertFrom-Json
+    }
+    else
+    {
+        $null
+    }
+}
+
+$Global:OSEnvJSON = global:get-osenvjson
+
+function global:auth 
+{
+    param (
+           $stage ,
+           $authName,          
+           $env,
+           [ValidateSet ('pacAuthSelected','pacAuthName','userName','applicationId','deviceCode','managedIdentity','githubFederated','azureDevOpsFederated')]
+           $authMethod,
+           $appId,
+           $tenantId,
+           $userName,
+           $cloud,
+           $secretName,
+           [switch] $renew
+           )
+
+    $envJson = global:get-osenvjson
+
+    $stage = if ($stage -ne $null) {$stage} else {if ($envJson.devops.defaultStage -ne $null) {$envJson.devops.defaultStage}else{'dev'}}
+
+    "stage is $stage"    
+
+    if ($envJson -ne $null)
+    {
+        $authJSON = ($envJson.devops.stages | Where-Object {$_.stage -eq $stage}).authSettings
+        $authJSON
+    }
+
+    $authName = if ($authName -ne $null) {$authName} else {if ($authJSON.authName -ne $null) {$authJSON.authName} else {$null}}
+    "authName is $authName"  
+
+    $authMethod = if ($authMethod -ne $null) {$authMethod} else {if ($authJSON.authMethod -ne $null) {$authJSON.authMethod}else{'pacAuthSelected'}} 
+    "authMethod is $authMethod" 
+
+    $env = if ($env -ne $null) {$env} else {if ($authJSON.environment -ne $null) {$authJSON.environment}else{$null}} 
+    "Environment is $env" 
+
+    $cloud = if ($cloud -ne $null) {$cloud} else {if ($authJSON.cloud -ne $null) {$authJSON.cloud}else{'Public'}} 
+    "cloud is $cloud"     
+
+
+    if ($env -eq $null){
+        throw "Environment must be specified using -env argument or the environments.json file."
+    }
+    elseif ($authMethod -eq 'pacAuthSelected')
+    {
+        Show-Shortcut-Note "pac org select -env $env"
+        pac org select -env $env
+
+        Show-Shortcut-Note "pac auth who"        
+        pac auth who
+        return
+    }
+    elseif ($authName -eq $null)
+    {
+        throw "An AuthName is required for the $authMethod authentication method."
+    }
+
+    $authDeleted = $false
+
+    Show-Shortcut-Note "pac auth select -n $authName"    
+    $($output = pac auth select -n $authName ) *>&1
+    if (($output -join ";") -like "*Error*")
+    {
+        # No Action Taken                    
+    }
+    elseif (-not $renew)
+    {
+
+        Show-Shortcut-Note "pac org select -env $env"
+        pac org select -env $env
+
+        Show-Shortcut-Note "pac auth who"        
+        pac auth who
+        return
+    }
+    else
+    {
+        Show-Shortcut-Note "pac auth delete -n $authName"
+        pac auth delete -n $authName
+        $authDeleted = $true
+    }
+ 
+    if ($authMethod -eq 'pacAuthName')
+    {
+           throw "The specified AuthName $authName does not  exist.  Please create it prior to using the $authMethod authentication method."
+    }
+    elseif ($authMethod -eq 'userName')
+    {
+     $userName = if ($userName -ne $null) {$userName} else {if ($authJSON.userName -ne $null) {$authJSON.userName}else{$null}}
+
+        if ($userName -eq $null)
+        {
+            Show-Shortcut-Note "pac auth create -n $authName -env $env --cloud $cloud"
+            pac auth create -n $authName -env $env --cloud $cloud
+        }
+        else
+        {
+            Show-Shortcut-Note "pac auth create -n $authName -env $env --cloud $cloud --username $userName"
+            pac auth create -n $authName -env $env --cloud $cloud -un $userName
+        }
+    }
+    elseif($authMethod -eq 'deviceCode')
+    {
+        Show-Shortcut-Note "pac auth create -n $authName -env $env --cloud $cloud --deviceCode"        
+        pac auth create -n $authName -env $env --cloud $cloud --deviceCode
+    }
+    else
+    {
+
+        $appId = if ($appId -ne $null) {$appId} else {if ($authJSON.applicationId -ne $null) {$authJSON.applicationId}else{$null}} 
+        "AppId is $appId" 
+
+        $tenantId = if ($tenantId -ne $null) {$tenantId} else {if ($authJSON.tenant -ne $null) {$authJSON.tenant}else{$null}} 
+        "tenantId is $tenantId" 
+
+        $secretName = if ($secretName -ne $null) {$secretName} else {if ($authJSON.secretName -ne $null) {$authJSON.secretName}else{$appId}} 
+        "secretName is $secretName" 
+
+        $clientSecret = ensureSecret -Name  $secretName 
+
+        # 'applicationId','deviceCode','managedIdentity','githubFederated','azureDevOpsFederated'       
+
+        if ($authMethod -eq 'applicationId'){
+            Show-Shortcut-Note "pac auth create -n $authName -env $env --cloud $cloud -id $appId -t $tenantId -cs **********************"        
+            pac auth create -n $authName -env $env --cloud $cloud -id $appId -t $tenantId -cs $clientSecret  
+        }
+        elseif ($authMethod -eq 'managedIdentity')
+        {
+            Show-Shortcut-Note "pac auth create -n $authName -env $env --cloud $cloud --managedIdentity -id $appId -t $tenantId -cs **********************"        
+            pac auth create -n $authName -env $env --cloud $cloud --managedIdentity -id $appId -t $tenantId -cs $clientSecret  
+        }
+        elseif ($authMethod -eq 'githubFederated')
+        {
+            Show-Shortcut-Note "pac auth create -n $authName -env $env --cloud $cloud --githubFederated -id $appId -t $tenantId -cs **********************"        
+            pac auth create -n $authName -env $env --cloud $cloud --githubFederated -id $appId -t $tenantId -cs $clientSecret  
+        }
+        elseif ($authMethod -eq 'azureDevOpsFederated')
+        {
+            Show-Shortcut-Note "pac auth create -n $authName -env $env --cloud --azureDevOpsFederated $cloud -id $appId -t $tenantId -cs **********************"        
+            pac auth create -n $authName -env $env --cloud $cloud --azureDevOpsFederated -id $appId -t $tenantId -cs $clientSecret  
+        }        
+   
+    }
+}
 
 function global:export {
     Show-Shortcut-Note "dotnet restore" 
